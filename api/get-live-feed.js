fe4e02f0c7957d8f7f77d4dd97f561da2427ff40
Prajwal18py <prajwala27112005@gmail.com>
2026-03-23 12:30:23 +0530

    fix 304 cache, no-cache headers, remove broken Batch feed

--- api/get-live-feed.js
+++ api/get-live-feed.js
@@ -128,6 +128,8 @@ async function fetchLatest() {
 
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate')
+  res.setHeader('Pragma', 'no-cache')
 
   try {
     // Check cache
